8950e37ea5b7d6d34fbe6f368205b32be6f6f4f1
aimanfadillah <mlakece914@gmail.com>
2022-11-06 11:36:51 +0700

    new

---
+++ produk.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="produk.css">
+    <title>Barang</title>
+</head>
+<body>
+    
+  <!-- navbar -->
+
+  <div class="navbar">
+        <h1>ShopMan</h1>
+        
+        <ul>
+            <li><a href="#">Login</a></li>
+        </ul>
+    </div>
+
+    <!-- main -->
+
+    <div class="produk">
+        <img src="jeruk.jpg" alt="jeruk">
+        <div class="penjualan">
+            <div class="list_produk">
+                <h1 class="judul">Jeruk dari tempat pilihan</h1>
+                <h1 class="harga">Rp50.000</h1>
+            </div>
+            <div class="list_akhir">
+                <a class="tombol_beli">Beli</a>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+</html>
