0fc6dc372c84c2ec393bf68b8d5ed6d837034e2c
aimanfadillah <mlakece914@gmail.com>
2022-11-28 16:05:11 +0700

    new

--- shop.sql
+++ shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 26, 2022 at 06:30 AM
+-- Generation Time: Nov 28, 2022 at 10:03 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.9
 
@@ -64,7 +64,7 @@ CREATE TABLE `komentar_60` (
 --
 
 INSERT INTO `komentar_60` (`id`, `nama`, `komentar`) VALUES
-(1, 'Gion', 'gila ada fitur login');
+(14, 'aiman', 'Real Smile\r\n');
 
 -- --------------------------------------------------------
 
@@ -77,14 +77,6 @@ CREATE TABLE `komentar_61` (
   `nama` varchar(50) DEFAULT NULL,
   `komentar` varchar(150) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `komentar_61`
---
-
-INSERT INTO `komentar_61` (`id`, `nama`, `komentar`) VALUES
-(1, 'Aiman', 'sama kayanya'),
-(2, 'Bejo', 'Loh iya kah');
 
 -- --------------------------------------------------------
 
@@ -103,7 +95,7 @@ CREATE TABLE `komentar_62` (
 --
 
 INSERT INTO `komentar_62` (`id`, `nama`, `komentar`) VALUES
-(1, 'Aiman', 'Tulip ip ip ip ip');
+(2, 'aiman', 'tulip ip ip\r\n');
 
 -- --------------------------------------------------------
 
@@ -122,8 +114,7 @@ CREATE TABLE `komentar_63` (
 --
 
 INSERT INTO `komentar_63` (`id`, `nama`, `komentar`) VALUES
-(1, 'aiman', 'boleh 😛😛😌'),
-(2, 'Faisal', 'Boleh');
+(1, 'aiman', 'boleh\r\n');
 
 -- --------------------------------------------------------
 
@@ -148,13 +139,6 @@ CREATE TABLE `komentar_65` (
   `nama` varchar(50) DEFAULT NULL,
   `komentar` varchar(150) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `komentar_65`
---
-
-INSERT INTO `komentar_65` (`id`, `nama`, `komentar`) VALUES
-(2, 'Aiman', 'ihh ubur ubur');
 
 -- --------------------------------------------------------
 
@@ -181,8 +165,8 @@ INSERT INTO `produk` (`id`, `produk`, `keterangan`, `harga`, `img`, `id_komen`, 
 (61, 'Jeruk bali', 'yoi', 5000, 'jeruk.jpg', '61', 'makanan'),
 (62, 'Tulip', 'Harga nya terjakau cuy', 2000, 'Tulips.jpg', '62', 'makanan'),
 (63, 'Bandar', 'yooo', 10000, 'Penguins.jpg', '63', 'hewan'),
-(64, 'Rumah', 'yap', 200000, 'Lighthouse.jpg', '64', 'Perabotan'),
-(65, 'JellyFish', 'yap', 1000000, 'Jellyfish.jpg', '65', 'hewan');
+(64, 'Rumah Ytta', 'ya', 2000000, 'Lighthouse.jpg', '64', 'perabotan'),
+(65, 'Jelly Fish', 'yooo', 140000, 'Jellyfish.jpg', '65', 'hewan');
 
 -- --------------------------------------------------------
 
@@ -201,9 +185,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `nama`, `password`) VALUES
-(1, 'aiman', '$2y$10$QB4UUT6HNO6KAuQ8G4Cz7e3lw55DGtKobc7k.nu03cTHKWI0/Bgzi'),
-(2, 'aldi', '$2y$10$D/FMBGIh18YhtXG1JIFREun4fUWFCGit7YdyjdBRVGDI324Ixkqwm'),
-(3, 'ilyas', '$2y$10$SfgiwycxFs8H0jOSOt6DdugF82/e52odPDw7t4vd2ypY59sLZe8Se');
+(638310, 'aiman', '$2y$10$Z.d9UCtPYkhbgJ/9Z5dEiOmhkPlP8.7T7HYrEMb8WmCsY1Fx8uFCi'),
+(6383143, 'ilyas', '$2y$10$PYWzJn2YoAIkLmaywLAaNOi0JqOAyH0CIm2JbU7kpSc6ac94TaQni');
 
 --
 -- Indexes for dumped tables
@@ -277,7 +260,7 @@ ALTER TABLE `kategori`
 -- AUTO_INCREMENT for table `komentar_60`
 --
 ALTER TABLE `komentar_60`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `komentar_61`
@@ -289,7 +272,7 @@ ALTER TABLE `komentar_61`
 -- AUTO_INCREMENT for table `komentar_62`
 --
 ALTER TABLE `komentar_62`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `komentar_63`
@@ -301,19 +284,13 @@ ALTER TABLE `komentar_63`
 -- AUTO_INCREMENT for table `komentar_64`
 --
 ALTER TABLE `komentar_64`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `komentar_65`
 --
 ALTER TABLE `komentar_65`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
